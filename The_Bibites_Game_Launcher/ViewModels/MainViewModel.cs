fad33a3247315353a4cde173d468c41da175b6d8
MeltingDiamond <adrian.oestrem@gmail.com>
2025-08-07 00:01:41 +0200

    Update UI

--- The_Bibites_Game_Launcher/ViewModels/MainViewModel.cs
+++ The_Bibites_Game_Launcher/ViewModels/MainViewModel.cs
@@ -4,6 +4,5 @@ namespace The_Bibites_Game_Launcher.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "Welcome to The Bibites Game Launcher!\nAs you can see it is not finished yet.";
+
 }
